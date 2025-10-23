240d47bbae582638c1e2c16b56c39f1a5da17292
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-10-23 12:49:28 -0300

    query productos

--- JavaScriptEnMongoShell/queryProductos.js
+++ JavaScriptEnMongoShell/queryProductos.js
@@ -14,4 +14,32 @@ Resolver utilizando forEach()
 ================================================================
 */
 
-//load("JavaScriptEnMongoShell\insertProductos.js")
+//load("JavaScriptEnMongoShell/insertProductos.js")
+
+print("--- Precio Mínimo ---");
+let precioMin = db.productos.aggregate([{$project : {nombre: 1,precio:1,_id:0}}, {$sort : {precio : 1}},{$limit : 1}]);
+precioMin.forEach(function(producto) {
+  print("El precio mínimo es: " + producto.precio);
+});
+
+print("\n--- Precio Promedio ---");
+let totalPrecio = 0;
+let count = 0;
+db.productos.find().forEach(function(producto) {
+  totalPrecio += producto.precio;
+  count++;
+});
+
+if (count > 0) {
+  print("El precio promedio es: " + (totalPrecio / count));
+} else {
+  print("No hay productos para calcular el promedio.");
+}
+
+print("\n--- Productos Ordenados (mayor a menor) ---");
+let prodsOrdenados = db.productos.aggregate([{$project : {nombre: 1,precio:1,categoria:1,_id:0}}, {$sort : {precio : -1}}]);
+prodsOrdenados.forEach(function(producto) {
+  printjson(producto);
+});
+
+//no supe hacer "Mostrar el producto más caro por cada categoría"
